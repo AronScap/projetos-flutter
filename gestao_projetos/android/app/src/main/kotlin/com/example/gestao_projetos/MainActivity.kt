5567eb9ca470afb127ff3faa420b28fd68be4077
Aron Scapinello Selhorst <aronscapinello@gmail.com>
2020-11-06 12:00:31 -0300

    06-11-2020

---
+++ gestao_projetos/android/app/src/main/kotlin/com/example/gestao_projetos/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.gestao_projetos
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ gestao_projetos/lib/main.dart
@@ -0,0 +1,179 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Login(),
+  ));
+}
+
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Gestão de Projetos Pessoais"),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text(
+                'Drawer Header',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.account_circle),
+              title: Text('Perfil'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Configurações'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.logout),
+              title: Text('Sair'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          
+        },
+        child: Icon(Icons.add),
+        backgroundColor: Colors.blueGrey,
+      ),
+      body: Padding(
+        padding: EdgeInsets.only(left: 20,right: 20),
+        child: ListView(
+          
+          children: <Widget>[
+            Card(
+              child: Row(
+                children: [
+                  Text("asasdasdsa")
+                ],
+              )
+            ),
+            Card(
+              child: ListTile(
+                leading: FlutterLogo(),
+                title: Text('One-line with leading widget'),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                title: Text('One-line with trailing widget'),
+                trailing: Icon(Icons.more_vert),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: FlutterLogo(),
+                title: Text('One-line with both widgets'),
+                trailing: Icon(Icons.more_vert),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: FlutterLogo(),
+                title: Text('One-line with both widgets'),
+                trailing: Icon(Icons.more_vert),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: FlutterLogo(),
+                title: Text('One-line with both widgets'),
+                trailing: Icon(Icons.more_vert),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: FlutterLogo(),
+                title: Text('One-line with both widgets'),
+                trailing: Icon(Icons.more_vert),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: FlutterLogo(),
+                title: Text('One-line with both widgets'),
+                trailing: Icon(Icons.more_vert),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: FlutterLogo(),
+                title: Text('One-line with both widgets'),
+                trailing: Icon(Icons.more_vert),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: FlutterLogo(),
+                title: Text('One-line with both widgets'),
+                trailing: Icon(Icons.more_vert),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: FlutterLogo(),
+                title: Text('One-line with both widgets'),
+                trailing: Icon(Icons.more_vert),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                title: Text('One-line dense ListTile'),
+                dense: true,
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: FlutterLogo(size: 56.0),
+                title: Text('Two-line ListTile'),
+                subtitle: Text('Here is a second line'),
+                trailing: Icon(Icons.more_vert),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: FlutterLogo(size: 72.0),
+                title: Text('Three-line ListTile'),
+                subtitle: Text(
+                  'A sufficiently long subtitle warrants three lines.'
+                ),
+                trailing: Icon(Icons.more_vert),
+                isThreeLine: true,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
